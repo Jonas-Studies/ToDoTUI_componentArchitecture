ac3de2f83cec935c8685469833cff9418f9177b3
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-17 19:28:50 +0100

    Added render functions for the field and edit one task application structs

--- src/tui/edit_one_task_application.rs
+++ src/tui/edit_one_task_application.rs
@@ -3,8 +3,10 @@ use crate::Task;
 use super::field::Field;
 
 use ratatui::{
+    prelude::Buffer,
     widgets::Paragraph,
-    layout::{Rect, Layout, Constraint}
+    layout::{Rect, Layout, Constraint},
+    style::{Stylize, Modifier}
 };
 
 pub struct Application <'applications_lifetime> {
@@ -16,7 +18,7 @@ impl Application <'_> {
     pub fn new (task_to_edit: & Task, area: Rect) -> Self {
         let [ status_area, name_area ] = Layout::vertical(
             [ Constraint::Length(1), Constraint::Length(1) ]
-        ).areas(area);
+        ).spacing(1).areas(area);
 
         let status = Field::new(
             status_area,
@@ -27,7 +29,7 @@ impl Application <'_> {
                 else {
                     String::from("Todo:")
                 }
-            )
+            ).add_modifier(Modifier::BOLD)
         );
 
         let name = Field::new(
@@ -36,5 +38,10 @@ impl Application <'_> {
         );
 
         Self { status, name }
+    }
+
+    pub fn render (& self, buffer: & mut Buffer) {
+        self.status.render(buffer);
+        self.name.render(buffer);
     }
 }
--- src/tui/field.rs
+++ src/tui/field.rs
@@ -1,6 +1,6 @@
 use super::traits::IsContent;
 
-use ratatui::prelude::Rect;
+use ratatui::prelude::{Rect, Buffer};
 
 pub struct Field <Content> where Content: IsContent {
     area: Rect,
@@ -10,5 +10,9 @@ pub struct Field <Content> where Content: IsContent {
 impl <Content> Field <Content> where Content: IsContent {
     pub fn new (area: Rect, content: Content) -> Self {
         Self { area, content }
+    }
+
+    pub fn render (& self, buffer: & mut Buffer) {
+        self.content.render_ref(self.area, buffer);
     }
 }
