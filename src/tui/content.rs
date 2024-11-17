73281b2d95e7e538b57bc7618e53a0feec9cab48
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-17 23:31:02 +0100

    Implemented a custom textfield as content

--- src/tui/content.rs
+++ src/tui/content.rs
@@ -1,5 +1,29 @@
 use super::traits::IsContent;
 
-use ratatui::widgets::Paragraph;
+use ratatui::{
+    prelude::{Buffer, Rect},
+    widgets::{WidgetRef, Block, Paragraph}
+};
 
 impl IsContent for Paragraph <'_> {}
+
+pub struct TextField <'textfields_lifetime> {
+    text: String,
+    borders: Block <'textfields_lifetime>
+}
+
+impl TextField <'_> {
+    pub fn new (text: String, title: String) -> Self {
+        let borders = Block::bordered().title(title);
+
+        Self { text, borders }
+    }
+}
+
+impl IsContent for TextField <'_> {}
+
+impl WidgetRef for TextField <'_> {
+    fn render_ref(& self, area: Rect, buffer: & mut Buffer) {
+        Paragraph::new(self.text.clone()).block(self.borders.clone()).render_ref(area, buffer);
+    }
+}
--- src/tui/edit_one_task_application.rs
+++ src/tui/edit_one_task_application.rs
@@ -1,6 +1,9 @@
 use crate::Task;
 
-use super::field::Field;
+use super::{
+    content::TextField,
+    field::Field
+};
 
 use ratatui::{
     prelude::Buffer,
@@ -11,13 +14,13 @@ use ratatui::{
 
 pub struct Application <'applications_lifetime> {
     status: Field <Paragraph <'applications_lifetime>>,
-    name: Field <Paragraph <'applications_lifetime>>
+    name: Field <TextField <'applications_lifetime>>
 }
 
 impl Application <'_> {
     pub fn new (task_to_edit: & Task, area: Rect) -> Self {
         let [ status_area, name_area ] = Layout::vertical(
-            [ Constraint::Length(1), Constraint::Length(1) ]
+            [ Constraint::Length(1), Constraint::Length(3) ]
         ).spacing(1).areas(area);
 
         let status = Field::new(
@@ -34,7 +37,7 @@ impl Application <'_> {
 
         let name = Field::new(
             name_area,
-            Paragraph::new(task_to_edit.get_name())
+            TextField::new(task_to_edit.get_name(), String::from("Name"))
         );
 
         Self { status, name }
