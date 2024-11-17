003d609b1147c3ea26ed8271d4e39bfc8f71ec3a
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-17 19:29:10 +0100

    Build a first usable tui app

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -5,10 +5,31 @@ mod edit_one_task_application;
 
 use crate::Task;
 
+use ratatui::crossterm::event::{self, Event, KeyEventKind, KeyCode};
+
 pub fn edit_one_task (task_to_edit: & mut Task) {
     let mut terminal = ratatui::init();
 
     let app = edit_one_task_application::Application::new(task_to_edit, terminal.get_frame().area());
+
+    terminal.draw(
+        |frame| {
+            app.render(frame.buffer_mut())
+        }
+    ).expect("Failed to draw the app to the terminal");
+
+    loop {
+        if let Event::Key(key) = event::read().expect("Error reading an event") {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Esc => {
+                        break;
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
 
     ratatui::restore();
 }
