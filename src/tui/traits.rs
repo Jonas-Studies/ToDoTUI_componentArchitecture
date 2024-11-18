9bf1d035828ed79ad230e76ad34069263ede18ea
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-18 22:55:16 +0100

    Added a trait to recieve the relative cursor position in one content, or none if theres none

--- src/tui/traits.rs
+++ src/tui/traits.rs
@@ -1,8 +1,15 @@
-use ratatui::widgets::WidgetRef;
+use ratatui::{
+    prelude::Position,
+    widgets::WidgetRef
+};
 
 pub trait IsContent: WidgetRef + CanBeFocused {}
 
 pub trait CanBeFocused {
     fn focus(& mut self);
     fn unfocus(& mut self);
+}
+
+pub trait MayDisplayCursor {
+    fn get_cursor_position (& self) -> Option<Position>;
 }
