a93e20d7085f77cb52c9ec865b96c269d51705fd
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 10:56:39 +0200

    Deleted the existing code to rewrite it from scratch

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,32 +1,18 @@
-mod traits;
 mod content;
-mod field;
-mod edit_one_task_application;
-mod select_one_task_application;
 
 use crate::Task;
 
 pub fn edit_one_task (task_to_edit: Task) -> Option<Task> {
     let mut terminal = ratatui::init();
     
-    let result = edit_one_task_application::Application::new(
-        task_to_edit,
-        terminal.get_frame().area()
-    ).run(& mut terminal);
-
     ratatui::restore();
 
-    return result;
+    return None;
 }
 
 pub fn select_one_task (tasks_to_select_from: & Vec<Task>) -> Option<usize> {
     let mut terminal = ratatui::init();
 
-    let result = select_one_task_application::Application::new(
-        tasks_to_select_from,
-        terminal.get_frame().area()
-    ).run(& mut terminal);
-
     ratatui::restore();
-    result
+    return None;
 }
